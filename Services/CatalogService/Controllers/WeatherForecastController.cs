f35fe44977c5bce7a4b2628586bf182b641328c7
gcrockenberg <gcrockenberg@hotmail.com>
2023-05-28 08:12:11 -0400

    Initital coffeeapi deploy to Azure

--- Services/CatalogService/Controllers/WeatherForecastController.cs
+++ Services/CatalogService/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace CatalogService.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        const string DEPL_SANITY_CHECK = "20230527";
+        const string DEPL_SANITY_CHECK = "20230528";
 
         private static readonly string[] Summaries = new[]
         {
