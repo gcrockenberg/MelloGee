bcc4d2d1db0c8579660cf7c98cf107c5f7904105
gcrockenberg <gcrockenberg@hotmail.com>
2023-05-30 10:10:17 -0400

    Switch to App Gateway, impl. health check

---
+++ Services/CatalogService/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Azure.Core;
+global using Azure.Core.Diagnostics;
+global using Azure.Identity;
+global using CatalogService.Database;
+global using CatalogService.Model;
+global using CatalogService.SyncDataServices.Http;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Diagnostics.HealthChecks;
--- Services/CatalogService/Program.cs
+++ Services/CatalogService/Program.cs
@@ -1,23 +1,15 @@
-using Azure.Core;
-using Azure.Core.Diagnostics;
-using Azure.Identity;
-using CatalogService.Database;
-using CatalogService.Model;
-using CatalogService.SyncDataServices.Http;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Vault:Name defined in appsettings.json
 var keyVaultEndpoint = new Uri($"https://{builder.Configuration["Vault:Name"]}.vault.azure.net/");
 if (builder.Environment.IsDevelopment())
 {
-    //
-    // The following is only to accommodate local dev Docker containers.
-    // Better to stick with DefaultAzureCredential but, in order to work on Docker,
-    // would have to include the larger az client in the image
-    // see https://github.com/Azure/azure-sdk-for-net/issues/19167
-    //
+    /**
+    * The following is only to accommodate local dev Docker containers.
+    * Better to stick with DefaultAzureCredential but, in order to work on Docker,
+    * would have to include the larger az client in the image
+    * see https://github.com/Azure/azure-sdk-for-net/issues/19167
+    */
     TokenCredential credential = new ClientSecretCredential(
         builder.Configuration["Vault:AZURE_TENANT_ID"],
         builder.Configuration["Vault:AZURE_CLIENT_ID"],
@@ -58,9 +50,12 @@ builder.Services.AddDbContext<Context>(options =>
 //options.UseSqlServer(builder.Configuration["defaultConnection"])   // From Key Vault
 );
 
+/// Call extension method to configure health check
+builder.Services.AddCustomHealthCheck(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 //if (app.Environment.IsDevelopment())
 //{
 app.UseSwagger();
@@ -73,3 +68,35 @@ app.MapControllers();
 
 app.Run();
 
+/// <summary>
+/// Extend Program.cs with some defaults
+/// </summary>
+public partial class Program
+{
+    public static string Namespace = typeof(Program).Assembly.GetName().Name;
+    public static string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+}
+
+/// <summary>
+/// Add some extension methods
+/// </summary>
+public static class CustomExtensionMethods
+{
+    /// <summary>
+    /// Implement health check
+    /// Best practice but also used by Azure App Gateway
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+    {
+        var hcBuilder = services.AddHealthChecks();
+
+        hcBuilder
+            .AddCheck("self", () => HealthCheckResult.Healthy());
+            
+        return services;
+    }
+
+}
