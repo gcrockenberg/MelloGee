bea65947c45dfeb136dc5e9a7a572f93546f44c1
gcrockenberg <gcrockenberg@hotmail.com>
2023-06-21 13:47:47 -0400

    Testing narrowed Action scope

--- Services/CatalogService/Controllers/WeatherForecastController.cs
+++ Services/CatalogService/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ namespace CatalogService.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        const string DEPL_SANITY_CHECK = "20230621.3";
+        const string DEPL_SANITY_CHECK = "20230621.4";
 
         private static readonly string[] Summaries = new[]
         {
