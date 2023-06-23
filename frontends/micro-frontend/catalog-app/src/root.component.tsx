a894dd0291819f736febb63d0f75b796d14779af
gcrockenberg <gcrockenberg@hotmail.com>
2023-06-22 21:38:33 -0400

    Testing single-spa

--- frontends/micro-frontend/catalog-app/src/root.component.tsx
+++ frontends/micro-frontend/catalog-app/src/root.component.tsx
@@ -2,6 +2,7 @@ export default function Root(props) {
   return (
     <section>
       <h1>Deployments can take 10 minutes to propagate to CDN</h1>
+      <h2>2023.06.22 21:20</h2>
       {props.name} is mounted! 
       <br />
       <a href="/welcome">Go to single-spa welcome</a>
