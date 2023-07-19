1c6c901e21d1c17e994997c49485adcf57b3f825
gcrockenberg <gcrockenberg@hotmail.com>
2023-07-19 12:13:41 -0400

    Playing with 3D

--- Frontends/Angular/Me/src/app/pages/3D/three-d/three-d.component.ts
+++ Frontends/Angular/Me/src/app/pages/3D/three-d/three-d.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import "external/ajax.googleapis.com_ajax_libs_model-viewer_3.1.1_model-viewer.min.js"
 
 @Component({
   selector: 'app-three-d',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './three-d.component.html',
-  styleUrls: ['./three-d.component.scss']
+  styleUrls: ['./three-d.component.scss'],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class ThreeDComponent {
-
-}
+export class ThreeDComponent { }
