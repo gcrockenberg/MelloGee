d02cea3411ed78dcc8e1ba7df47ec3b06b61952c
gcrockenberg <gcrockenberg@hotmail.com>
2023-07-29 14:52:37 -0400

    Order

--- Services/Order/Order.API/Controllers/CheckoutApiController.cs
+++ Services/Order/Order.API/Controllers/CheckoutApiController.cs
@@ -1,38 +1,47 @@
 using System.ComponentModel.Design;
+using System.Text.Json;
 
 [Route("api/v1/[controller]")]
 [ApiController]
 public class OrderController : Controller
 {
-    private readonly ILogger<OrderController> _logger;
-    private readonly IConfiguration _configuration;
+  private readonly ILogger<OrderController> _logger;
+  private readonly IConfiguration _configuration;
 
 
-    public OrderController(ILogger<OrderController> logger, IConfiguration configuration)
+  public OrderController(ILogger<OrderController> logger, IConfiguration configuration)
+  {
+    this._logger = logger;
+    this._configuration = configuration;
+  }
+
+
+  [Route("create-checkout-session")]
+  [HttpPost]
+  public ActionResult<KeyValuePair<string, string>> Create()
+  {
+    // Header passing impacted by APIM
+    _logger.LogInformation("--> Headers: {headers}", JsonSerializer.Serialize(Request.Headers));
+    // Request.Headers["Referer"].ToString() -> http://localhost:4200/
+    // or the production web app url
+    var domain = Request.Headers.Referer;
+
+    if (string.IsNullOrWhiteSpace(domain))
     {
-        this._logger = logger;
-        this._configuration = configuration;
+      // Request.Headers["Origin"].ToString() -> http://localhost:4200
+      domain = $"{Request.Headers.Origin}/";
     }
 
-
-    [Route("create-checkout-session")]
-    [HttpPost]
-    public ActionResult<KeyValuePair<string, string>> Create()
+    var options = new SessionCreateOptions
     {
-        // Request.Headers["Referer"].ToString() -> http://localhost:4200/
-        // or the production web app url
-        var domain = Request.Headers.Referer;
-
-        var options = new SessionCreateOptions
-        {
-            // domain includes the trailing "/"
-            SuccessUrl = domain + "catalog",
-            CancelUrl = domain + "catalog",
-            PaymentMethodTypes = new List<string>
+      // domain includes the trailing "/"
+      SuccessUrl = domain + "catalog",
+      CancelUrl = domain + "catalog",
+      PaymentMethodTypes = new List<string>
             {
               "card"
             },
-            LineItems = new List<SessionLineItemOptions>
+      LineItems = new List<SessionLineItemOptions>
             {
               new SessionLineItemOptions
               {
@@ -48,15 +57,15 @@ public class OrderController : Controller
                 Quantity = 2
               },
             },
-            Mode = "payment",
-        };
+      Mode = "payment",
+    };
 
-        var service = new SessionService();
-        Session session = service.Create(options);
+    var service = new SessionService();
+    Session session = service.Create(options);
 
-        KeyValuePair<string, string> kvp = new KeyValuePair<string, string>("url", session.Url);
-        // Response.Headers.Add("Location", session.Url);
-        // return new StatusCodeResult(303);
-        return Ok(kvp);
-    }
+    KeyValuePair<string, string> kvp = new KeyValuePair<string, string>("url", session.Url);
+    // Response.Headers.Add("Location", session.Url);
+    // return new StatusCodeResult(303);
+    return Ok(kvp);
+  }
 }
