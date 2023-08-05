7da6d5e57fe0bb3bbed029044fad26afd86bd61d
gcrockenberg <gcrockenberg@hotmail.com>
2023-08-04 21:44:25 -0400

    Google analytics

--- Frontends/Angular/Me/src/app/app.component.ts
+++ Frontends/Angular/Me/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CartSidebarComponent } from "./components/cart-sidebar/cart-sidebar.component";
 import { AltFooterComponent } from "./shared/components/alt-footer/alt-footer.component";
+
+declare const gtag: Function;   // Google analytics
 
 @Component({
     selector: 'app-root',
@@ -24,4 +26,14 @@ import { AltFooterComponent } from "./shared/components/alt-footer/alt-footer.co
 })
 export class AppComponent {
   title: string = 'me'; 
+
+  constructor(private _router: Router) {
+
+    // Report page navigations to Google analytics
+    _router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-HVZMVYPD4C', { 'page_path': event.urlAfterRedirects });
+      }      
+    })    
+  }
 }
